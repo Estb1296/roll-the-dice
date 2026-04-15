9548b6b56b024b228ae0ca7bbb57600945a8506b
ezrateshale <ezra.teshale12@gmail.com>
2026-04-15 11:52:56 -0500

    feat:DiceRoll App complete reformat Static

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,12 +1,45 @@
 package com.pluralsight;
 
 public class Dice {
+    private int maxValue;
+    private int minValue;
+    private static int randomNumber;
+
 
     public Dice() {
+    this.maxValue=6;
+    this.minValue=1;
+    randomNumber=0;
 
     }
 
-    public int roll(int maxValue, int minValue) {
-        return (int) (Math.random() * maxValue) + minValue;
+    public static int roll(int maxValue, int minValue) {
+        randomNumber= (int) (Math.random() * maxValue) + minValue;
+        return randomNumber;
     }
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public void setMaxValue(int maxValue) {
+        this.maxValue = maxValue;
+    }
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public void setMinValue(int minValue) {
+        this.minValue = minValue;
+    }
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+
+
 }
